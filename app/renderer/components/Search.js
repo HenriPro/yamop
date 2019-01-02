f2c73a11f70fbd0a555eaee28803bb67ba97c9b1
Henri Pietila <henrismail@gmail.com>
2019-01-02 09:24:43 -0800

    adjusted layout

--- app/renderer/components/Search.js
+++ app/renderer/components/Search.js
@@ -2,22 +2,24 @@ import React from 'react';
 
 export default function Search(props){
     return (
-        <span>
-            <h3>Search</h3>
-            <input 
-                type="text"
-                onChange={(e) => props.handleSearchInput(e.target.value)}
-                value={props.searchValue} 
-            />
-            <select
-                onChange={(e) => props.handleSearchType(e.target.value)}
-                value={props.searchTypeValue}
-            >
-                <option value="searchAll">Search All</option>
-                <option value="artist">Artist</option>
-                <option value="album">Album</option>
-                <option value="genre">Genre</option>
-            </select>
-        </span>
+        <h3>
+            <span>
+                Search 
+                <input 
+                    type="text"
+                    onChange={(e) => props.handleSearchInput(e.target.value)}
+                    value={props.searchValue} 
+                />
+                <select
+                    onChange={(e) => props.handleSearchType(e.target.value)}
+                    value={props.searchTypeValue}
+                >
+                    <option value="searchAll">Search All</option>
+                    <option value="artist">Artist</option>
+                    <option value="album">Album</option>
+                    <option value="genre">Genre</option>
+                </select>
+            </span>
+        </h3>
     );
 }
